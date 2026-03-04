f1ce973e778de62b75aa9ed1bdbbd9e0c0b9a739
Varun Saravanan <varunvsaran@utexas.edu>
2026-03-03 19:44:28 -0600

    Update solution for expected-value-discrete

--- expected-value-discrete/expected-value-discrete.py
+++ expected-value-discrete/expected-value-discrete.py
@@ -5,11 +5,14 @@ def expected_value_discrete(x, p):
     Returns: float expected value
     """
     
+    
     # Write code here
     x = np.asarray(x)
     p = np.asarray(p)
 
-    expected = x @ p
     if not np.isclose(p.sum(), 1.0, atol=10e-6):
-        raise ValueError("got error")
+        raise ValueError("probabilities don't sum to 1")
+    
+    expected = x @ p
+    
     return expected
